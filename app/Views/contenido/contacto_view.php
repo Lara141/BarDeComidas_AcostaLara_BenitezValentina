9bff2845e0b1efd0be0961a850a71cb9e2eabef9
Lara141 <acostalara.d.i@gmail.com>
2025-04-14 21:36:25 -0300

    contacto listo

--- app/Views/contenido/contacto_view.php
+++ app/Views/contenido/contacto_view.php
@@ -41,7 +41,7 @@ p{
             Looks good!
           </div>
         </div>
-        
+
         <div class="col-md-6 position-relative">
           <label for="validationTooltipProvincia" class="form-label color-fuente">Provincia</label>
           <select class="form-select" id="validationTooltipProvincia" required>
@@ -92,39 +92,56 @@ p{
           </div>
         </div>
         <div class="col-12">
-         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalFormulario">
-  Enviar Formulario
-</button>
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalFormulario">
+            Enviar Formulario
+          </button>
         </div>
-        
+
       </form>
+    </div>
+    <div class="col-md-6">
+      <h3 class="color-titulo">CONTACTO</h3>
+      <p > estos son los horario en los cual el restaurante esta abierto, y el telefono para contactar con el encargado, para sus dudas inmediatas y reservas</p>
+      <p>
+  <p class="fw-bold">Horarios:</p>
+  <p>-De Lunes a Viernes: 10 am a 14 pm - 17 pm a 22 pm </p>
+  <p>-Fines de Semana: 8 am a 23 pm </p>
+      <p>
+  <span class="fw-bold">Teléfono:</span> +54 3795-684058
+</p>
+<p>
+  <span style="font-weight: bold;">Direccion:</span> Av. 3 de abril y chacabuco
+</p>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Nuestra Ubicación</h2>
+      <div class="map-responsive">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3540.184587945613!2d-58.8383989!3d-27.4606587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456ca8983b9021%3A0x4c8289c24c020a8!2sPlaza%2025%20de%20Mayo!5e0!3m2!1ses-AR!2sar!4v1713141508765!5m2!1ses-AR!2sar"
+          width="600"
+          height="450"
+          style="border:0;"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   </div>
 </div>
 
-
-<!--Modal Formulario-->
 
 <div class="modal fade" id="modalFormulario" tabindex="-1" aria-labelledby="modalFormularioLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      
+
       <div class="modal-body">
-        <p>Su formulario a sido eviado, pronto responderemos a sus dudas</p>
+        <p>Su formulario ha sido enviado, pronto responderemos a sus dudas</p>
       </div>
-      
-    </div>
-  </div>
-</div>
-
-
-<!--Lado Derecho-->
-
-<div class="container">
-  <div class="row justify-content-end">
-    <div class="col-md-6">
-      <h3 class="color-titulo">Dejanos tus datos</h3>
-      <p >Para que un encargado pueda responder sus preguntas, por favor déjenos sus datos y sus dudas. Muchas gracias.</p>
 
     </div>
   </div>
