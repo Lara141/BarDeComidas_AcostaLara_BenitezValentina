31aeab6d1f24a9d000007327ec91e4b6fc96e0bb
Valentina-Benitez <valenbenitez246@gmail.com>
2025-04-28 16:00:09 -0300

    popila

--- app/Views/contenido/principal.php
+++ app/Views/contenido/principal.php
@@ -72,7 +72,7 @@
   </style>
 
 
-</head>
+
 
   <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
   <div class="carousel-indicators">
@@ -84,13 +84,13 @@
   </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="assets/img/carne1.jpg" class="d-block w-100" alt="...">
+      <img src="assets/img/bienveBar.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
       <img src="assets/img/empanadass.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="assets/img/dulceDeLeche.jpg" class="d-block w-100" alt="...">
+    <img src="assets/img/dulceDeLeche.jpg" class="d-block w-100" alt="...">
     </div>
    
   </div>
