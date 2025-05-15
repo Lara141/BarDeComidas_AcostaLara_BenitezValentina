269e36d72e537d08800fe13b23d31ddfa9f1730c
Lara141 <acostalara.d.i@gmail.com>
2025-05-15 10:58:11 -0300

    m

--- app/Views/contenido/contacto_view.php
+++ app/Views/contenido/contacto_view.php
@@ -1,12 +1,4 @@
-<?php if (! empty($errors)): ?>
-    <div class="alert alert-danger" role="alert">
-        <ul>
-        <?php foreach ($errors as $error): ?>
-            <li><?= esc($error) ?></li>
-        <?php endforeach ?>
-        </ul>
-    </div>
-<?php endif ?>
+
 <div class="container">
   <div class="row">
     <div class="col-md-6">
--- app/Views/contenido/registro.php
+++ app/Views/contenido/registro.php
@@ -1,12 +1,4 @@
-<?php if (! empty($errors)): ?>
-    <div class="alert alert-danger" role="alert">
-        <ul>
-        <?php foreach ($errors as $error): ?>
-            <li><?= esc($error) ?></li>
-        <?php endforeach ?>
-        </ul>
-    </div>
-<?php endif ?>
+
 <style>
     body {
         font-family: Arial, sans-serif;
