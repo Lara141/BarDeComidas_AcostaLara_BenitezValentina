557f5e7802dcd99abaca3a5f54c1c1b31afcd8ce
Lara141 <acostalara.d.i@gmail.com>
2025-06-03 09:39:24 -0300

    j

--- app/Controllers/producto_controller.php
+++ app/Controllers/producto_controller.php
@@ -5,61 +5,61 @@ use App\Models\Categoria_Model;
 
 class producto_controller extends BaseController{
 
-public function form_agregar_producto(){
-    $categoria= new Categoria_Model();
-    $data['categorias']= $categoria->findAll();
-    $dara['titulo']= 'Agregar producto ';
-    return view('administrador/encabezado_admin', $data).view('administrador/barraNav_admin').view('administrador/agregar_producto');
-}
-public function registrar_producto(){
-//procesa los datos del producto enviados por el formulario
-$validation= \Config\Services::validation();
-$request= \Config\Services::request();
+    public function form_agregar_producto(){
+        $categoria= new Categoria_Model();
+        $data['categorias']= $categoria->findAll();
+        $dara['titulo']= 'Agregar producto ';
+        return view('administrador/encabezado_admin', $data).view('administrador/barraNav_admin').view('administrador/agregar_producto');
+    }
+    public function registrar_producto(){
+    //procesa los datos del producto enviados por el formulario
+    $validation= \Config\Services::validation();
+    $request= \Config\Services::request();
 
-$validation->setRules(
-[
-//reglas de validacion
-'nombre'=>'required|max_length[255]',
-'precio'=>'required|decimal',
-'descripcion'=>'required|max_length[1000]',
-'estado'=>'required|integer',
-'stock'=>'required|integer',
-'categoria' => 'required',
-'imagen'=>'uploaded[imagen]|max_size[imagen, 4096]|ext_in[imagen,jpg,jpeg,png,gif]',
-],
-[//errors
-//completar los mensajes
-'nombre' => [
+    $validation->setRules(
+    [
+        //reglas de validacion
+        'nombre'=>'required|max_length[255]',
+        'precio'=>'required|decimal',
+        'descripcion'=>'required|max_length[1000]',
+        'estado'=>'required|integer',
+        'stock'=>'required|integer',
+        'categoria' => 'required',
+        'imagen'=>'uploaded[imagen]|max_size[imagen, 4096]|ext_in[imagen,jpg,jpeg,png,gif]',
+    ],
+    [//errors
+       //completar los mensajes
+        'nombre' => [
             'required' => 'El nombew es requerido',
         ],
-'precio' => [
+        'precio' => [
             'required' => 'El precio es requerido',
         ],
-'descripcion' => [
+        'descripcion' => [
             'required' => 'la descripcion es requerido',
         ],
-'estado' => [
+        'estado' => [
             'required' => 'El estado es requerido',
         ],
-'stock' => [
+        'stock' => [
             'required' => 'El stock es requerido',
         ],
 
-'categoria'=>[
-    'is_not_unique'=> 'Debe seleccionar una categoria',
+        'categoria'=>[
+            'is_not_unique'=> 'Debe seleccionar una categoria',
 
-],
-'imagen'=>[
-    'uploaded'=> 'Debe seleccionar una imagen',
-   'ext_in'=> 'debe ser una imagen valida',
+        ],
+        'imagen'=>[
+            'uploaded'=> 'Debe seleccionar una imagen',
+            'ext_in'=> 'debe ser una imagen valida',
 
-],
+        ],
 
-]
+    ]
 
-);
+    );
 
-if($validation->withRequest($request)->run()){
+    if($validation->withRequest($request)->run()){
     $img=$this->request->getFile('imagen');
     $nombre_aleatorio = $img->getRandomName();
     $img->move(ROOTPATH.'public/asset/upload', $nombre_aleatorio);
@@ -80,7 +80,7 @@ if($validation->withRequest($request)->run()){
     $producto->insert($data);
     return redirect()->route('agregar_producto')->with('mensaje', 'el producto se registro correctamente!');
 
-}else{
+    }else{
     $categoria=new Categoria_model();
     $data['validation']= $validation->getErrors();
     $data['categorias']=$categoria->findAll();
@@ -88,8 +88,17 @@ if($validation->withRequest($request)->run()){
 
     return view('administrador/encabezado_admin',$data).view('administrador/barraNav_admin').view('administrador/agregar_producto');
 
-}
+    }
 
 }
 
+function gestionar_producto(){
+    $producto_model= new peoducto_model();
+    $categoria=new Categoria_Model();
+
+    $data['producto']= producto_model->join('producto_categoria','producto_categoria.categoria_id=producto.producto_categoria')->findAll();
+    $data['titulo']='listar productos';
+
+    return view('administrador/encabezado_admin',$data).view('administrador/barraNav_admin
+}
 }
