f538d0a39e2c085dc23cf821caed8c487aca1bff
Lara141 <acostalara.d.i@gmail.com>
2025-06-10 10:13:48 -0300

    control listo

--- app/Controllers/producto_controller.php
+++ app/Controllers/producto_controller.php
@@ -113,7 +113,74 @@ function editar_producto($id=null){
 
 function actualizar_producto(){
 $request=\Config\Services::request();
+ $validation= \Config\Services::validation();
+
+
+ 
+    $validation->setRules(
+    [
+        //reglas de validacion
+        'nombre'=>'required|max_length[255]',
+        'precio'=>'required|decimal',
+        'descripcion'=>'required|max_length[1000]',
+        'estado'=>'required|integer',
+        'stock'=>'required|integer',
+        'categoria' => 'required',
+        'imagen'=>'uploaded[imagen]|max_size[imagen, 4096]|ext_in[imagen,jpg,jpeg,png,gif]',
+    ],
+     [//errors
+       //completar los mensajes
+        'nombre' => [
+            'required' => 'El nombew es requerido',
+        ],
+        'precio' => [
+            'required' => 'El precio es requerido',
+        ],
+        'descripcion' => [
+            'required' => 'la descripcion es requerido',
+        ],
+        'estado' => [
+            'required' => 'El estado es requerido',
+        ],
+        'stock' => [
+            'required' => 'El stock es requerido',
+        ],
+
+        'categoria'=>[
+            'is_not_unique'=> 'Debe seleccionar una categoria',
+
+        ],
+        'imagen'=>[
+            'uploaded'=> 'Debe seleccionar una imagen',
+            'ext_in'=> 'debe ser una imagen valida',
+
+        ],
+
+    ]
+
+    );
+
 //validar los datos
+//si pasa la validacion
+ $id= $request->getPost('id');
+
+ $data=[
+
+        'nombre_producto'=> $request->getPost('nombre'),
+        'precio_producto'=> $request->getPost('precio'),
+        'descripcion_producto'=> $request->getPost('descripcion'),
+        'estado_producto'=> 1,
+        'stock_producto'=> $request->getPost('stock'),
+        'categoria_id'=> $request->getPost('categoria'),
+        'imagen_producto'=> $nombre_aleatorio, 
+        
+    ];
+
+    $producto=new producto_model();
+    $producto->update($id, $data);
+    // mensaje que se actualizo correctamente el producto
+     return redirect()->route('gestionar')->with('mensaje', 'el producto se actualizo correctamente!');
+
 
 }
 
